10d6eb06000198b5872b2c5b45012f2bec702f40
Abhishek Sharma <akbhardwaj2329@gmail.com>
2022-09-25 22:42:01 +0530

    init

---
+++ app/src/main/java/com/example/digihelper/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.digihelper;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+
+    }
+}
