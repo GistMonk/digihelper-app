7e5ab139f33ff280d6493116f4feee34d11de2b1
Abhishek Sharma <akbhardwaj2329@gmail.com>
2022-09-26 22:33:34 +0530

    otp screen integrated

--- app/src/main/java/com/example/digihelper/OtpActivity.java
+++ app/src/main/java/com/example/digihelper/OtpActivity.java
@@ -1,25 +1,37 @@
 package com.example.digihelper;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
-
+import android.widget.Toast;
 
 
 public class OtpActivity extends AppCompatActivity {
 
-    TextView phoneNumberTxt;
+    EditText optText;
+    AppCompatButton verifyBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_otp);
 
-        phoneNumberTxt = (TextView) findViewById(R.id.phoneNumberT);
+        verifyBtn = (AppCompatButton) findViewById(R.id.verifyButton);
 
-        phoneNumberTxt.setText(getIntent().getStringExtra("PhoneNumber"));
+        verifyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                optText = (EditText) findViewById(R.id.otpTextField);
+                String otpTextString = optText.getText().toString();
+                Toast.makeText(getApplicationContext(), "otp is "+otpTextString,Toast.LENGTH_LONG).show();
+            }
+        });
 
     }
 }
